8d0d02bb8fc9c77acbbc73542633eeb4a763a77a
Shibu <shibu.a5859@gmail.com>
2023-12-24 12:49:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'gill-sans': ['Gill Sans', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
